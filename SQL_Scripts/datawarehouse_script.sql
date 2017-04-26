f0296440e3d4535ecea6b4ca162e7af58b5f1658
xCloudx8 <danielestrife@gmail.com>
2017-04-26 19:47:34 +0200

    v2.0

--- SQL_Scripts/datawarehouse_script.sql
+++ SQL_Scripts/datawarehouse_script.sql
@@ -1,32 +1,39 @@
-DROP TABLES IF EXISTS varianti, snp, term, term_definition;
+DROP TABLES IF EXISTS ft_variants, dt_positions, dt_term, dt_refalt ;
 
-CREATE TABLE varianti(
+CREATE TABLE ft_variants( --Fact table
 
-frequenze VARCHAR(300),
-chr VARCHAR(300),
-ref VARCHAR(300),
-alt VARCHAR(300),
-snp_ip INT(30),
-id INT(30)
+  phenotype VARCHAR(300) REFERENCES dt_term(all),
+  ref VARCHAR(300) REFERENCES dt_refalt(ref),
+  pos_chr VARCHAR(300) REFERENCES dt_positions(pos_chr),
 
-);
-
-CREATE TABLE snp(
-
-snp_ip VARCHAR(300) REFERENCES varianti(snp_id)
+  tot_var INTEGER(300),
 
 );
 
-CREATE TABLE term(
+CREATE TABLE dt_refalt( -- Dimension table
 
-nome VARCHAR(300),
-id INT(30) REFERENCES varianti(id)
+  ref VARCHAR(300) PRIMARY KEY,
+  refalt VARCHAR(300)
 
 );
 
-CREATE TABLE term_definition(
+CREATE TABLE dt_positions(
 
-term_id VARCHAR(300) REFERENCES term(id),
-term_definition VARCHAR(500)
+  pos_chr VARCHAR(300) PRIMARY KEY,
+  pos_int INT(30)
+
+);
+
+CREATE TABLE dt_term(
+
+  all VARCHAR(300) PRIMARY KEY,
+  system VARCHAR(500),
+  sub_system VARCHAR(300) ,
+  general_definition VARCHAR(300),
+  sub_category VARCHAR(300) ,
+  area_target VARCHAR(500),
+  focus_target VARCHAR(300) ,
+  Process_abnormalities_focus_target VARCHAR(500),
+  phenotype VARCHAR(300)
 
 );
